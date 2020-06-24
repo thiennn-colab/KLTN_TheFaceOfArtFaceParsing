16ef61b5234590904b97a2e60f5bde784bc56de6
Thien Nguyen <nguyenngocthien024@gmail.com>
2020-06-24 09:04:36 +0700

    asd

--- Updates/face_parsing/test_1.py
+++ Updates/face_parsing/test_1.py
@@ -94,8 +94,8 @@ def vis_parsing_maps(im, style, parsing_anno, stride, save_im=False, save_path='
 
     num_of_class = np.max(vis_parsing_anno)
 
-    texture_hair = getTexture('E:\\KLTN\\cfeapi\\Updates\\face_parsing\\textures\\' + _styleArr[style], (512, 512))
-    texture = getTexture('E:\\KLTN\\cfeapi\\Updates\\face_parsing\\textures\\' + _styleArr[style], (512, 512))
+    texture_hair = getTexture('/home/KLTN_TheFaceOfArtFaceParsing/Updates/face_parsing/textures/' + _styleArr[style], (512, 512))
+    texture = getTexture('/home/KLTN_TheFaceOfArtFaceParsing/Updates/face_parsing/textures/' + _styleArr[style], (512, 512))
     lips_eyes = np.ones_like(im).astype(np.uint8)*255
     # skin 1, nose 10, upper_lip 12, lower-lip 13
     for pi in [1, 10]:

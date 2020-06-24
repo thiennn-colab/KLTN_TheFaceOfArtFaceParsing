4691eb159c4b4f392b95793d0a15dd662fa45409
Thien Nguyen <nguyenngocthien024@gmail.com>
2020-06-24 11:18:32 +0700

    'asd'

--- Updates/face_parsing/test_1.py
+++ Updates/face_parsing/test_1.py
@@ -146,8 +146,9 @@ def vis_parsing_maps(im, style, parsing_anno, stride, save_im=False, save_path='
     mask1 = lips_eyes
     src1 = im.copy().astype('uint8')[:, :, ::-1]
 
-    mask1 = float(mask1/255)
-    dst = int(src1 * mask1 + src2 * (1 - mask1))
+    # mask1 = mask1/255
+    # dst = src1 * mask1 + src2 * (1 - mask1)
+    dst = src1
 
     # lips_eyes_applyMask = applyMask(lips_eyes)
     result = vis_im.copy()

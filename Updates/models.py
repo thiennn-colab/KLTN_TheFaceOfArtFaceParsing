0225bcda9b9cae03a21ea304ab54e18136d287c4
Thien Nguyen <nguyenngocthien024@gmail.com>
2020-06-25 21:15:10 +0700

    asd

--- Updates/models.py
+++ Updates/models.py
@@ -1,18 +1,18 @@
-from django.db import models
-from django.conf import settings
+# from django.db import models
+# from django.conf import settings
 
-# Create your models here.
-
-
-def upload_update_image(instance, filename):
-    return "updates/{user}/{filename}".format(user=instance.user, filename=filename)
+# # Create your models here.
 
 
-class Update(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    content = models.TextField(blank=True, null=True)
-    image = models.ImageField(
-        upload_to=upload_update_image, blank=True, null=True)
+# def upload_update_image(instance, filename):
+#     return "updates/{user}/{filename}".format(user=instance.user, filename=filename)
 
-    def __str__(self):
-        return self.content
+
+# class Update(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+#     content = models.TextField(blank=True, null=True)
+#     image = models.ImageField(
+#         upload_to=upload_update_image, blank=True, null=True)
+
+#     def __str__(self):
+#         return self.content
